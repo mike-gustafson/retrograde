274d7f2892eba4f21b155321a654f77f7366a476
mike-gustafson <gustafson.m@gmail.com>
2024-03-10 15:59:14 -0700

    feat: start cleaning up js functions

--- utils/parse-platform-gamename-object into strings.js
+++ utils/parse-platform-gamename-object into strings.js
@@ -1,21 +1,20 @@
 const { Platform, Game } = require('../models');
 
 async function parsePlatformGamenameObjectIntoStrings(data) {
-    const platformAndGameNames = [];
-    if (!data) return platformAndGameNames;
-    const platformAndGameIds = Object.entries(data);
-    for (let i = 0; i < platformAndGameIds.length; i++) {
-        const platformId = platformAndGameIds[i][0];
-        const gameIds = platformAndGameIds[i][1];
+    const names = [];
+    if (!data) return names;
+    const ids = Object.entries(data);
+    for (let i = 0; i < ids.length; i++) {
+        const platformId = ids[i][0];
+        const gameIds = ids[i][1];
         const platform = await Platform.findByPk(platformId);
         for (let j = 0; j < gameIds.length; j++) {
             const gameId = gameIds[j];
             const game = await Game.findByPk(gameId);
-            platformAndGameNames.push([platform.name, game.name]);
+            names.push([platform.name, game.name]);
         }
     }
-    console.log(platformAndGameNames)
-    return platformAndGameNames;
+    return names;
 }
 
 module.exports = parsePlatformGamenameObjectIntoStrings;
